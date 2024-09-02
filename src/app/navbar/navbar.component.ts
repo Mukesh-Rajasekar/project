a97276d838607603bdd3f6eb9733c54a2b2fdc29
Bhavyanth <bhavyanth167@gmail.com>
2024-09-02 11:39:18 +0530

    Some front-end Changes done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginService } from '../login.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(private router: Router) {}
+export class NavbarComponent implements OnInit {
+  isLoggedIn = false;
+
+  constructor(private router: Router,private loginService:LoginService) {}
+
+  ngOnInit() {
+    this.checkLoginStatus();
+  }
+
+  checkLoginStatus() {
+    // Assuming your LoginService has a method to check if the user is logged in
+    this.isLoggedIn = this.loginService.isLoggedIn();
+  }
 
   navigateToLogin() {
     this.router.navigate(['/login']);
@@ -16,5 +27,11 @@ export class NavbarComponent {
 
   navigateToSignup() {
     this.router.navigate(['/signup']);
+  }
+
+  logout() {
+    this.loginService.logout();
+    this.isLoggedIn = false;
+    this.router.navigate(['/login']);
   }
 }
