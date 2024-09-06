d04da131bdc6f1c3b70b1c549ff5d89406573aa0
Bhavyanth <bhavyanth167@gmail.com>
2024-09-06 08:36:35 +0530

    added marquee tag

--- src/app/dummy/dummy.component.ts
+++ src/app/dummy/dummy.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { LoginService } from '../login.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -8,56 +8,51 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   templateUrl: './dummy.component.html',
   styleUrls: ['./dummy.component.css']
 })
-export class DummyComponent {
-  favRest: any[] = [];
-  constructor(private user: UserService, private logins: LoginService,private snackBar: MatSnackBar) { }
+export class DummyComponent implements OnInit {
+  favRest: any[] = []; // Initialize with an empty array
+  hasFavorites: boolean = false; // A boolean flag to check if there are favorites
+
+  constructor(private user: UserService, private logins: LoginService, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    console.log('working');
+    this.loadFavorites();
+  }
+
+  // Function to load favorite restaurants
+  loadFavorites(): void {
     let id = this.logins.getUserId();
     this.user.getFavRestaurants().subscribe({
       next: (data) => {
         this.favRest = data;
-        console.log(this.favRest);
+        this.hasFavorites = this.favRest.length > 0; // Update hasFavorites flag based on data
+        console.log('Favorite restaurants:', this.favRest);
       },
-      error: () => { console.log("error occured"); }
-    })
-  }
-
-
-
-  deleteFav(restaurantId: any) {
-    console.log("Working: " + restaurantId);
-    this.user.deleteFavRest(restaurantId).subscribe({
-      next: (data) => {
-        console.log("Deleted favorite restaurant");
-        this.refreshFavorites();
-        this.snackBar.open('Favorite restaurant deleted successfully!', ' ', {
-          duration: 5000,
-          verticalPosition: 'bottom',
-        });
-      },
-      // error: () => {
-      //   this.snackBar.open('Error occurred while deleting favorite restaurant.', 'Close', {
-      //     duration: 3000,
-      //     verticalPosition: 'bottom',
-      //     horizontalPosition: 'right',
-      //   });
-      // }
-    });
-  }
-  
-  refreshFavorites() {
-    let id = this.logins.getUserId();
-    this.user.getFavRestaurants().subscribe({
-      next: (data) => {
-        this.favRest = data;
-        console.log(this.favRest);
-      },
-      error: () => {
-        console.log("Error occurred while fetching favorite restaurants");
+      error: () => { 
+        console.log("Error occurred while fetching favorite restaurants"); 
+        this.hasFavorites = false; // Set to false if error occurred
       }
     });
   }
 
+  // Function to delete favorite restaurant
+  deleteFav(restaurantId: any): void {
+    console.log("Deleting restaurant with ID: " + restaurantId);
+    this.user.deleteFavRest(restaurantId).subscribe({
+      next: () => {
+        console.log("Deleted favorite restaurant");
+        this.snackBar.open('Favorite restaurant deleted successfully!', ' ', {
+          duration: 5000,
+          verticalPosition: 'bottom',
+        });
+        this.loadFavorites(); // Refresh the favorite list after deletion
+      },
+      error: () => {
+        this.snackBar.open('Error occurred while deleting favorite restaurant.', 'Close', {
+          duration: 3000,
+          verticalPosition: 'bottom',
+          horizontalPosition: 'right',
+        });
+      }
+    });
+  }
 }
